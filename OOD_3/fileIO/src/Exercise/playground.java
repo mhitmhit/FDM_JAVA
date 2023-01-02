06fbffefc5200075c068b2873aa3c6233dfe749a
Y.E <yassine.elkabaili@gmail.com>
2023-01-02 10:40:50 -0500

    add a playground in ood3 for some testing

---
+++ OOD_3/fileIO/src/Exercise/playground.java
@@ -0,0 +1,13 @@
+package Exercise;
+
+import java.util.List;
+
+public class playground {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> myList = null;
+		System.out.println(myList.size());
+	}
+
+}
