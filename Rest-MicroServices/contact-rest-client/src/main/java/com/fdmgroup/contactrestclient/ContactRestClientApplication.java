ed168c7a85d8ca242467514bb5dbb8e21660a999
YE <yassine.elkabaili@gmail.com>
2023-02-14 15:35:10 -0500

    Rest client fix 2-14-23 3:35pm

--- Rest-MicroServices/contact-rest-client/src/main/java/com/fdmgroup/contactrestclient/ContactRestClientApplication.java
+++ Rest-MicroServices/contact-rest-client/src/main/java/com/fdmgroup/contactrestclient/ContactRestClientApplication.java
@@ -1,12 +1,8 @@
 package com.fdmgroup.contactrestclient;
 
-import java.net.http.HttpHeaders;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -26,12 +22,11 @@ public class ContactRestClientApplication {
 		return WebClient.builder();
 	}
 	
-	@Bean
-	public WebClient webClient(WebClient.Builder builder ) {
-		return builder.baseUrl("http://localhost:8080")
-				.defaultHeader(HttpHeaders,
-						MediaType.APPLICATION_JSON)
-				.build();
-	}
+   @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl(CONTACT_API_ROOT)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
 
 }
